c4c4a62b0d938059ffcdc38824282c84014e1572
CPallaresU <85590939+CPallaresU@users.noreply.github.com>
2022-06-12 23:43:10 -0500

    Add files via upload

--- Outliers.py
+++ Outliers.py
@@ -11,6 +11,10 @@ import statistics
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 def fila(x):
     return math.ceil(x/28)-1
@@ -39,9 +43,13 @@ c16=[]
 
 cont=0
 
+
+df = pd.read_csv("5_mice.csv")
+
+
 for i in range(1):
     
-    df = pd.read_csv("9_mice.csv")
+    #df = pd.read_csv("9_mice.csv")
 
     for k in df.columns:
         
@@ -155,27 +163,53 @@ for i in range(1,17):
        acum_mean = acum_mean + df[str(j)].mean()
        lista_std.append(df[str(j)].std())
        
-   list_std.append(statistics.stdev(lista_std))
+   if len(dict_.get(str(i))) == 1:
+       list_std.append(dict_.get(str(i))[0])
+   else:    
+       list_std.append(statistics.stdev(lista_std))
    lista_std=[]
-   lista_mean.append(acum_mean / len(dict_.get(str(i)))) #Lista de todos las medias por cuadrante de imagen
+   lista_mean.append(acum_mean / len(dict_.get(str(i)))) #Lista de todos las medias por cuadrante, son 16 posiciones solamente.
    
    
 list_img = []
+
+
+df["C1"] = 500*[0]
+df["C2"] = 500*[0]
+df["C3"] = 500*[0]
+df["C4"] = 500*[0]
+df["C5"] = 500*[0]
+df["C6"] = 500*[0]
+df["C7"] = 500*[0]
+df["C8"] = 500*[0]
+df["C9"] = 500*[0]
+df["C10"] = 500*[0]
+df["C11"] = 500*[0]
+df["C12"] = 500*[0]
+df["C13"] = 500*[0]
+df["C14"] = 500*[0]
+df["C15"] = 500*[0]
+df["C16"] = 500*[0]
+
+
 
 for i in range(0,len(df)):
     
    means_ = []
 
    for j in range(1,17):
+       
+       str_c = "C"+str(j)
        acum_ = 0
        for k in dict_.get(str(j)): #Calculo de media por cuadrante a partir de la lista ya calculada de medias
            
            acum_ = acum_ + df[str(k)][i]            
-
+       
+       df[str_c][i] = acum_  / len(dict_.get(str(j)))
        means_.append(acum_  / len(dict_.get(str(j)))) ##Lista de media por cuadrante
    
    check_mean_per_q = []
-   list_img.append(mean_squared_error(lista_mean,means_))
+   list_img.append(mean_squared_error(lista_mean,means_)) #Que tan cercanos están a los valores de lista_mean
    
    #for n in range(16):
        #check_mean_per_q.append(((means_[n] - lista_mean[n])*(means_[n] - lista_mean[n]))/ 2)
@@ -183,7 +217,7 @@ for i in range(0,len(df)):
 
 
 
-plt.plot(list_img,list(np.arange(500)))
+#plt.plot(list_img,list(np.arange(500)))
 ## El eje X me muestra el error promedio de cada imagen con respecto al
 ## valor medio de cada cuadrante original vs el calculado de cada imagen
 
@@ -193,15 +227,84 @@ plt.plot(list_img,list(np.arange(500)))
 
 cont = 0
 print(statistics.mean(list_img) + statistics.stdev(list_img))
+outliers = []
+not_outliers = []
 for k in range(500):
     if list_img[k] < statistics.mean(list_img) + statistics.stdev(list_img) :
+        not_outliers.append(k)
         cont=cont+1
-print(cont)    
+    else:
+        outliers.append(k)
+    
+print(cont)  
+  
+
+"""
+
+df["PROMEDIO"] = list_img
+df.iloc[outliers].plot(y=["C1","C2","C3","C5","C6","C7","C8","C9"])
+df.iloc[not_outliers].tail(len(outliers)).plot(y=["C1","C2","C3","C5","C6","C7","C8","C9"])
+
+plt.plot(df.iloc[not_outliers].tail(len(outliers))["PROMEDIO"],outliers)
+
+plt.plot(df.iloc[outliers]["PROMEDIO"],outliers)
+
+"""    
+
+#GRÁFICO DE DENDISDAD
+
+
+dq = pd.read_csv("9_mice.csv")        
+
+for l in outliers:    
+    row = dq.iloc[0]
+    row.plot(kind='density',color = "y")
+
+dq = pd.read_csv("8_mice.csv")        
+
+for l in outliers:    
+    row = dq.iloc[0]
+    row.plot(kind='density',color = "b")
+    
+
+
+#RELLENAR DATASET PARA RECONSTRUCCIÓN DE LAS IMAGENES NORMALES Y DE OUTLIERS
+
+
+
+def imagenes_reconstruct(idx_img, img_, dir_,y):
             
-            
-            
-            
-            
-            
-            
-            
+    lista_ = [0]*784
+    cont = 0
+    for k in idx_img:
+        if any(c.isalpha() for c in k) == False:
+            lista_[int(k)] = int(abs(img_[cont]*256))
+            cont = cont + 1
+    
+    
+    pixels = lista_
+    
+    # Use PIL to create an image from the new array of pixels
+    
+    
+    image_out = Image.new(mode="1",size=(28,28))
+    image_out.putdata(pixels)
+    
+    image_out.save(dir_+"/"+str(y)+'.png')
+
+
+for y in range(len(outliers)):
+
+    idx_img=list(df.iloc[outliers[y]].index)
+    img_ = list(df.iloc[outliers[y]])
+    imagenes_reconstruct(idx_img,img_,"outliers",y)
+
+
+for w in range(len(df)):
+    
+    if w not in outliers:
+        idx_img=list(df.iloc[w].index)
+        img_ = list(df.iloc[w])
+        imagenes_reconstruct(idx_img,img_,"normal",w)
+
+          
